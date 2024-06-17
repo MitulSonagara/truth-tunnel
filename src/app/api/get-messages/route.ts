eecc49fc608e2f9b1e06ab0a302cec989308944a
Mitul Sonagara <mitulkumarsongara@gmail.com>
2024-06-17 12:39:44 +0530

    get-messages

---
+++ src/app/api/get-messages/route.ts
@@ -0,0 +1,52 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
+import { User } from "next-auth";
+import mongoose from "mongoose";
+
+export async function GET(request: Request) {
+    await dbConnect()
+
+    const session = await getServerSession(authOptions)
+
+    const user: User = session?.user
+
+    if (!session || !session.user) {
+        return Response.json({
+            success: false,
+            message: "Not authenticated"
+        }, { status: 401 })
+    }
+
+    const userId = new mongoose.Types.ObjectId(user._id);
+
+    try {
+        const user = await UserModel.aggregate([
+            { $match: { id: userId } },
+            { $unwind: '$messages' },
+            { $sort: { 'messages.createdAt': -1 } },
+            {$group:{_id:'_id',messages:{$push:'$messages'}}}
+        ])
+        
+        if (!user) {
+            return Response.json({
+                success: false,
+                message: "User not found"
+            }, { status: 401 })
+        }
+
+        return Response.json({
+            success: true,
+            messages: user[0].messages
+        }, { status: 200 })
+
+
+    } catch (error) {
+        console.log("Error in getting message acceptance status")
+        return Response.json({
+            success: false,
+            message: "Error in getting message acceptance status"
+        }, { status: 500 })
+    }
+}
