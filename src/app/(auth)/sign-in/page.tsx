553578236eb26d0666c55d05f04ed0c912a181c5
Mitul Sonagara <mitulkumarsongara@gmail.com>
2024-06-30 12:37:35 +0530

    sign_in done

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,11 +1,111 @@
-import React from 'react';
+/* eslint-disable react-hooks/rules-of-hooks */
+"use client";
 
-const page = () => {
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import Link from "next/link";
+import {signIn} from "next-auth/react"
+import { useEffect, useState, useCallback } from "react";
+import { useDebounce } from 'use-debounce';
+import { useToast } from "@/components/ui/use-toast";
+import { useRouter } from "next/navigation";
+import { signUpSchema } from "@/schemas/signUpSchema";
+import axios, { AxiosError } from "axios";
+import { ApiResponse } from "@/types/ApiResponse";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Loader2 } from "lucide-react";
+import { signInSchema } from "@/schemas/signInSchema";
+
+const Page = () => {
+    const { toast } = useToast();
+    const router = useRouter();
+
+    //zod implementation
+    const form = useForm<z.infer<typeof signInSchema>>({
+        resolver: zodResolver(signInSchema),
+        defaultValues: {
+            identifier: "",
+            password: "",
+        },
+    });
+
+    const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+        const result = await signIn('credentials', {
+            redirect:false,
+            identifier: data.identifier,
+            password:data.password
+        })
+
+        if (result?.error) {
+            toast({
+                title: "Login Failed",
+                description: "Incorrect username or password",
+                variant:"destructive"
+            })
+        }
+
+        if (result?.url) {
+            router.replace("/dashboard")
+        }
+    };
+
     return (
-        <div>
-            fdasfa
+        <div className="flex justify-center items-center min-h-screen bg-gray-100">
+            <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+                <div className="text-center">
+                    <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+                        Join <br />Truth-Tunnel
+                    </h1>
+                    <p className="mb-4">
+                        Sign Ip to start your anonymous adventure.
+                    </p>
+                </div>
+
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+                        <FormField
+                            name="identifier"
+                            control={form.control}
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Email/Username</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="Enter Email/Username" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            name="password"
+                            control={form.control}
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Password</FormLabel>
+                                    <FormControl>
+                                        <Input type="password" placeholder="Enter Password" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <Button type="submit">
+                            Sign In
+                        </Button>
+                    </form>
+                </Form>
+                <div className="text-center mt-4">
+                    <p>
+                        Don{"'"}t have an account?{' '}
+                        <Link href="/sign-up" className="text-blue-600 hover:text-blue-800">Sign Up</Link>
+                    </p>
+                </div>
+            </div>
         </div>
     );
-}
+};
 
-export default page;
+export default Page;
