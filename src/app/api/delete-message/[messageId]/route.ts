130840c63e434f3afce7151f74a21de279ff0c0f
Mitul Sonagara <mitulkumarsongara@gmail.com>
2024-07-02 10:07:58 +0530

    delete-msg route

---
+++ src/app/api/delete-message/[messageId]/route.ts
@@ -0,0 +1,45 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../auth/[...nextauth]/options";
+import { User } from "next-auth";
+
+export async function DELETE(request: Request, { params }: { params: { messageId: string } }) {
+    const messageId = params.messageId
+    await dbConnect()
+
+    const session = await getServerSession(authOptions)
+
+    const user: User = session?.user
+
+    if (!session || !session.user) {
+        return Response.json({
+            success: false,
+            message: "Not authenticated"
+        }, { status: 401 })
+    }
+
+    try {
+        const updateResult = await UserModel.updateOne(
+            { _id: user._id },
+            { $pull: { messages: { _id: messageId } } }
+        )
+        if (updateResult.modifiedCount == 0) {
+            return Response.json({
+                success: false,
+                message: "Message not found or already deleted"
+            }, { status: 404 })
+        } else {
+            return Response.json({
+                success: true,
+                message: "Message deleted"
+            }, { status: 200 })
+        }
+    } catch (error) {
+        console.log("Errors in deleting route")
+        return Response.json({
+            success: false,
+            message: "Error deleting message"
+        }, { status: 500 })
+    }
+}
