a3131fa0c0bcb74be2722bfaaf9e56d6c182f198
Jayanth <jayanththalla33@gmail.com>
2024-10-02 08:21:24 +0530

    added logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,36 +7,51 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 const Navbar = () => {
-    const { data: session, status } = useSession();
-    const user: User | undefined = session?.user as User;
+  const { data: session, status } = useSession();
+  const user: User | undefined = session?.user as User;
 
-    // Determine if session is loading, logged in, or logged out
-    const loading = status === "loading";
-    const loggedIn = !!session;
+  // Determine if session is loading, logged in, or logged out
+  const loading = status === "loading";
+  const loggedIn = !!session;
 
-    return (
-        <>
-            <nav>
-                <div className="flex justify-between p-8 border items-center md:px-28">
-                    {loading ? (
-                        <h1 className="font-bold text-2xl">Loading...</h1>
-                    ) : loggedIn ? (
-                        <>
-                            <h1 className="font-bold text-2xl md:text-3xl">Welcome, {user?.username || user?.email}</h1>
-                                <Button onClick={() => signOut()} className="w-full md:w-auto rounded-xl" >Sign Out</Button>
-                        </>
-                    ) : (
-                        <>
-                            <h1 className="font-bold text-2xl">Truth Tunnel</h1>
-                            <Link href="/sign-in">
-                                        <Button className="w-full md:w-auto rounded-xl">Sign In</Button>
-                            </Link>
-                        </>
-                    )}
-                </div>
-            </nav>
-        </>
-    );
+  return (
+    <>
+      <nav>
+        <div className="flex justify-between p-8 border items-center md:px-28">
+          {loading ? (
+            <h1 className="font-bold text-2xl">Loading...</h1>
+          ) : loggedIn ? (
+            <>
+              <h1 className="font-bold text-2xl md:text-3xl">
+                Welcome, {user?.username || user?.email}
+              </h1>
+              <Button
+                onClick={() => signOut()}
+                className="w-full md:w-auto rounded-xl"
+              >
+                Sign Out
+              </Button>
+            </>
+          ) : (
+            <>
+              <div className="flex items-center">
+                <Image
+                  src="/assets/logo1.png"
+                  alt="Truth Tunnel Logo"
+                  width={180}
+                  height={180}
+                  className="mr-2"
+                />
+              </div>
+              <Link href="/sign-in">
+                <Button className="w-full md:w-auto rounded-xl">Sign In</Button>
+              </Link>
+            </>
+          )}
+        </div>
+      </nav>
+    </>
+  );
 };
 
 export default Navbar;
