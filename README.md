e9c22f68dec3934ecce5cd21add12fbcf35d92ff
Priyanshu Verma <priyanshuverma@outlook.in>
2024-10-06 06:41:47 +0000

    fix: authentication and emailing

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 Welcome to the Truth-Tunnel Web Application! This platform allows users to send and receive messages securely while maintaining anonymity. The application features a robust login system with OTP (One-Time Password) verification to ensure the safety and privacy of user identities.
 
-You can preview and try the website live in Vercel here : 
+You can preview and try the website live in Vercel here :
 
 ## Table of Contents
 
@@ -27,65 +27,81 @@ You can preview and try the website live in Vercel here :
 ## Installation
 
 ## Prerequisites
+
 - Node.js
 - npm
 
 ## Steps
 
 1. Clone the repository:
-  
-  
+
 ```bash
 git clone https://github.com/MitulSonagara/truth-tunnel.git
 cd truth-tunnel
 ```
+
 2. Install dependencies:
+
 ```bash
 npm install
-  ```
+```
+
 3. Set up environment variables:
-   
+
    Create a .env file in the root directory and configure the following variables:
+
 ```bash
 MONGODB_URI=""
 NEXTAUTH_SECRET=""
+EMAIL_USER=""
 EMAIL_PASS=""
-  ```
+```
+
 4. Build the application:
-   
+
 ```bash
 npm run build
 ```
+
 5. Run the application:
 
 ```bash
 npm start
-  ```
+```
+
 ## Usage
+
 1. Register: Create a new account using an anonymous identity.
 2. Login: Enter your credentials and verify your identity using the OTP sent to your registered email.
 3. Send Messages: Compose and send encrypted messages to other users.
 4. Receive Messages: View received messages in your inbox, decrypted and secure.
 
 # Screenshots
+
 ## Home Page
+
 ![image](https://github.com/MitulSonagara/truth-tunnel/blob/master/public/assets/home.png)
 
-
 ## Registration Page
+
 ![image](https://github.com/MitulSonagara/truth-tunnel/blob/master/public/assets/signup.png)
 
 ## OTP Verification
+
 ![image](https://github.com/MitulSonagara/truth-tunnel/blob/master/public/assets/otp.png)
 
 ## Messaging Interface
+
 ## Copy the unique link
+
 ![image](https://github.com/MitulSonagara/truth-tunnel/blob/master/public/assets/dashboard.png)
 
 ## send messages to user without login
+
 ![image](https://github.com/MitulSonagara/truth-tunnel/blob/master/public/assets/send-msg.png)
 
 ## Technologies Used
+
 - Frontend:
   - NextJs
   - HTML/CSS
@@ -95,7 +111,7 @@ npm start
   - Node.js
   - Express.js
 - Database:
-  - MongoDB 
+  - MongoDB
 - Security:
 
   - next/auth for authentication
@@ -103,19 +119,21 @@ npm start
   - bcrypt (Squared the password)
 
 ## Contributing
+
 We welcome contributions! Please follow these steps:
 
 1. Fork the repository.
-2. Create a new branch 
+2. Create a new branch
 3. Make your changes.
 4. Commit your changes (git commit -m 'Add some feature').
 5. Push to the branch (git push origin feature-branch).
 6. Open a Pull Request.
 
 ## Contact
+
 For any queries or issues, please reach out to:
 
-- Link:  [mitulsonagara.com](https://portfolio-phi-topaz-23.vercel.app/)
+- Link: [mitulsonagara.com](https://portfolio-phi-topaz-23.vercel.app/)
 - GitHub: [MitulSonagara](https://github.com/MitulSonagara)
 
 ## Made with ❤️ by Mitul Sonagara.
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -6,160 +6,204 @@ import { useForm } from "react-hook-form";
 import * as z from "zod";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import { useDebounce } from 'use-debounce';
-import { toast } from 'sonner';
+import { useDebounce } from "use-debounce";
+import { toast } from "sonner";
 import { useRouter } from "next/navigation";
 import { signUpSchema } from "@/schemas/signUpSchema";
 import axios, { AxiosError } from "axios";
 import { ApiResponse } from "@/types/ApiResponse";
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Loader2 } from "lucide-react";
 
 const Page = () => {
-    const [username, setUsername] = useState("");
-    const [usernameMessage, setUsernameMessage] = useState("");
-    const [isCheckingUsername, setIsCheckingUsername] = useState(false);
-    const [isSubmitting, setIsSubmitting] = useState(false);
-    const [debouncedUsername] = useDebounce(username, 300);
-    const router = useRouter();
+  const [username, setUsername] = useState("");
+  const [usernameMessage, setUsernameMessage] = useState("");
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [debouncedUsername] = useDebounce(username, 300);
+  const router = useRouter();
 
-    // Zod implementation
-    const form = useForm<z.infer<typeof signUpSchema>>({
-        resolver: zodResolver(signUpSchema),
-        defaultValues: {
-            username: "",
-            email: "",
-            password: "",
-        },
-    });
+  // Zod implementation
+  const form = useForm<z.infer<typeof signUpSchema>>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
 
-    useEffect(() => {
-        const checkUsernameUnique = async () => {
-            if (debouncedUsername) {
-                setIsCheckingUsername(true);
-                setUsernameMessage('');
-                try {
-                    const response = await axios.get(`/api/check-username-unique?username=${debouncedUsername}`);
-                    console.log(response);
-                    
-                    let msg = response.data.message
-                    setUsernameMessage(msg);
-                } catch (error) {
-                    const axiosError = error as AxiosError<ApiResponse>;
-                    setUsernameMessage(axiosError.response?.data.message ?? "Error checking username");
-                } finally {
-                    setIsCheckingUsername(false);
-                }
-            }
-        };
-        checkUsernameUnique();
-    }, [debouncedUsername]);
-
-    const onSubmit = async (data: z.infer<typeof signUpSchema>) => {
-        setIsSubmitting(true);
+  useEffect(() => {
+    const checkUsernameUnique = async () => {
+      if (debouncedUsername) {
+        setIsCheckingUsername(true);
+        setUsernameMessage("");
         try {
-            const response = await axios.post<ApiResponse>('/api/sign-up', data);
-            toast.success('Success', { description: response.data.message });
-            router.replace(`/verify/${data.username}`);
+          const response = await axios.get(
+            `/api/check-username-unique?username=${debouncedUsername}`
+          );
+          console.log(response);
+
+          let msg = response.data.message;
+          setUsernameMessage(msg);
         } catch (error) {
-            console.error("Error in signup of user", error);
-            const axiosError = error as AxiosError<ApiResponse>;
-            let errorMessage = axiosError.response?.data.message || "An error occurred"; // Fallback error message
-            toast.error("Sign-up failed", { description: errorMessage });
+          const axiosError = error as AxiosError<ApiResponse>;
+          setUsernameMessage(
+            axiosError.response?.data.message ?? "Error checking username"
+          );
+          form.setError("username", {
+            message:
+              axiosError.response?.data.message ?? "Error checking username",
+          });
         } finally {
-            setIsSubmitting(false);
+          setIsCheckingUsername(false);
         }
+      }
     };
+    checkUsernameUnique();
+  }, [debouncedUsername]);
 
-    return (
-        <div className="flex justify-center items-center min-h-screen">
-            <div className="w-full max-w-md p-8 space-y-8 rounded-lg shadow-md border rounded-3xl"> {/* Fixed spacing issue */}
-                <div className="text-center">
-                    <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
-                        Join <br />Truth-Tunnel
-                    </h1>
-                    <p className="mb-4">
-                        Sign up to start your anonymous adventure.
-                    </p>
-                </div>
+  const onSubmit = async (data: z.infer<typeof signUpSchema>) => {
+    setIsSubmitting(true);
+    try {
+      const response = await axios.post<ApiResponse>("/api/sign-up", data);
+      toast.success("Success", { description: response.data.message });
+      router.replace(`/verify/${data.username}`);
+    } catch (error) {
+      console.error("Error in signup of user", error);
+      const axiosError = error as AxiosError<ApiResponse>;
+      let errorMessage =
+        axiosError.response?.data.message || "An error occurred"; // Fallback error message
+      toast.error("Sign-up failed", { description: errorMessage });
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
 
-                <Form {...form}>
-                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-                        <FormField
-                            name="username"
-                            control={form.control}
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Username</FormLabel>
-                                    <FormControl>
-                                        <Input
-                                            className="rounded-xl"
-                                            placeholder="Enter Username"
-                                            {...field}
-                                            onChange={(e) => {
-                                                field.onChange(e);
-                                                setUsername(e.target.value);
-                                                setUsernameMessage(''); // Clear message on input change
-                                            }}
-                                        />
-                                    </FormControl>
-                                    {isCheckingUsername && <Loader2 className="animate-spin" />}
-                                    <FormMessage>
-                                        <span className={usernameMessage === "Username is available" ? "text-green-600" : "text-red-600"}>
-                                            {usernameMessage}
-                                        </span>
-                                    </FormMessage>
-                                </FormItem>
-                            )}
-                        />
-                        <FormField
-                            name="email"
-                            control={form.control}
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Email</FormLabel>
-                                    <FormControl>
-                                        <Input className="rounded-xl" placeholder="Enter Email" {...field} />
-                                    </FormControl>
-                                    <FormMessage>{form.formState.errors.email?.message}</FormMessage> {/* Display error message */}
-                                </FormItem>
-                            )}
-                        />
-                        <FormField
-                            name="password"
-                            control={form.control}
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Password</FormLabel>
-                                    <FormControl>
-                                        <Input className="rounded-xl" type="password" placeholder="Enter Password" {...field} />
-                                    </FormControl>
-                                    <FormMessage>{form.formState.errors.password?.message}</FormMessage> {/* Display error message */}
-                                </FormItem>
-                            )}
-                        />
-                        <Button type="submit" disabled={isSubmitting} className="rounded-xl">
-                            {isSubmitting ? (
-                                <>
-                                    <Loader2 className="mr-4 h-4 w-4 animate-spin" />Please wait
-                                </>
-                            ) : (
-                                "Sign Up"
-                            )}
-                        </Button>
-                    </form>
-                </Form>
-                <div className="text-center mt-4">
-                    <p>
-                        Already a member?{' '}
-                        <Link href="/sign-in" className="text-blue-600 hover:text-blue-800">Sign In</Link>
-                    </p>
-                </div>
-            </div>
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="w-full max-w-md p-8 space-y-8  shadow-md border rounded-3xl">
+        {/* Fixed spacing issue */}
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Join <br />
+            Truth-Tunnel
+          </h1>
+          <p className="mb-4">Sign up to start your anonymous adventure.</p>
         </div>
-    );
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              name="username"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input
+                      className="rounded-xl"
+                      placeholder="Enter Username"
+                      {...field}
+                      onChange={(e) => {
+                        field.onChange(e);
+                        setUsername(e.target.value);
+                        setUsernameMessage(""); // Clear message on input change
+                      }}
+                    />
+                  </FormControl>
+                  {isCheckingUsername && <Loader2 className="animate-spin" />}
+                  <FormMessage>
+                    <span
+                      className={
+                        usernameMessage === "Username is available"
+                          ? "text-green-600"
+                          : "text-red-600"
+                      }
+                    >
+                      {usernameMessage}
+                    </span>
+                  </FormMessage>
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="email"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      className="rounded-xl"
+                      placeholder="Enter Email"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage>
+                    {form.formState.errors.email?.message}
+                  </FormMessage>{" "}
+                  {/* Display error message */}
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      className="rounded-xl"
+                      type="password"
+                      placeholder="Enter Password"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage>
+                    {form.formState.errors.password?.message}
+                  </FormMessage>{" "}
+                  {/* Display error message */}
+                </FormItem>
+              )}
+            />
+            <Button
+              type="submit"
+              disabled={isSubmitting}
+              className="rounded-xl"
+            >
+              {isSubmitting ? (
+                <>
+                  <Loader2 className="mr-4 h-4 w-4 animate-spin" />
+                  Please wait
+                </>
+              ) : (
+                "Sign Up"
+              )}
+            </Button>
+          </form>
+        </Form>
+        <div className="text-center mt-4">
+          <p>
+            Already a member?{" "}
+            <Link href="/sign-in" className="text-blue-600 hover:text-blue-800">
+              Sign In
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
