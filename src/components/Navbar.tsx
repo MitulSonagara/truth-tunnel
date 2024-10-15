f59a1a9f2f2b30cbb60a7b4ea44682e87a4792d7
MIGHTY1o1 <shubhagarwalcse@gmail.com>
2024-10-15 22:34:52 +0530

    add the navbar to the code

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,6 @@
 import React from "react";
 import Link from "next/link";
 import { useSession, signOut } from "next-auth/react";
-import { User } from "next-auth";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { ModeToggle } from "./ThemeToggle";
@@ -20,49 +19,38 @@ const Navbar = () => {
     resolvedTheme === "dark" ? "/assets/logo1.png" : "/assets/logo.png";
 
   return (
-    <>
-      <nav>
-        <div className="flex bg-gray-200 dark:bg-transparent 
-        shadow-xl justify-between p-8 border items-center md:px-28">
-          <div className="flex items-center">
-            <Link href="/">
-              <Image
-                src={logoSrc}
-                alt="Truth Tunnel Logo"
-                width={180}
-                height={180}
-                className="mr-2"
-              />
+    <nav className="bg-gray-100 dark:bg-transparent shadow-md border-b">
+      <div className="flex justify-between items-center p-3 md:px-16">
+        <Link href="/">
+          <Image
+            src={logoSrc}
+            alt="Truth Tunnel Logo"
+            width={120}
+            height={120}
+            className="mr-2 cursor-pointer"
+          />
+        </Link>
+        <div className="flex items-center space-x-3">
+          {loading ? (
+            <p className="text-xs text-gray-500">Loading...</p>
+          ) : loggedIn ? (
+            <Button
+              onClick={() => signOut()}
+              className="text-xs px-3 py-1 rounded-md bg-red-600 hover:bg-red-700 text-white transition-all duration-200"
+            >
+              Sign Out
+            </Button>
+          ) : (
+            <Link href="/sign-in">
+              <Button className="text-xs px-3 py-1 rounded-md bg-red-600 hover:bg-red-700 text-white transition-all duration-200">
+                Sign In
+              </Button>
             </Link>
-          </div>
-          <div className="flex items-center space-x-2">
-            {loading ? (
-              <>
-                <p>loading</p>
-              </>
-            ) : loggedIn ? (
-              <>
-                <Button
-                  onClick={() => signOut()}
-                  className="w-full md:w-auto rounded-xl"
-                >
-                  Sign Out
-                </Button>
-              </>
-            ) : (
-              <>
-                <Link href="/sign-in">
-                  <Button className="w-full md:w-auto rounded-xl">
-                    Sign In
-                  </Button>
-                </Link>
-              </>
-            )}
-            <ModeToggle />
-          </div>
+          )}
+          <ModeToggle />
         </div>
-      </nav>
-    </>
+      </div>
+    </nav>
   );
 };
 
