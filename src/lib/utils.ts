c38292abd4b2948234df8ffb3e34283e9a18a762
Priyanshu Verma <priyanshuverma@outlook.in>
2024-10-17 06:04:46 +0000

    feat: uses index DB

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,14 +1,15 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 import { decryptMessage } from "./crypto";
+import { getPrivateKey } from "./indexedDB";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
 
-export const renderDecryptedMessage = (message: string) => {
-  const privateKey = typeof window !== "undefined" ? localStorage.getItem("privateKey") : null;
+export const renderDecryptedMessage = async (message: string) => {
+  const privateKey = await getPrivateKey();
   if (privateKey) {
     return decryptMessage(privateKey, message);
   } else {
