81dcd9ee50da1c69a1d975629d4a0e381bf8a87e
Aryan Ramesh Jain <138214350+jainaryan04@users.noreply.github.com>
2024-10-18 22:55:32 +0530

    Fixes #162: Sign In Page Layout for Mobile View

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -82,7 +82,7 @@ const Page = () => {
         {/* Outer container */}
         <div
 
-          className="w-full bg-gray-200
+          className="mt-10 w-full bg-gray-200
          dark:bg-transparent max-w-md border-gray-200 
 
                  transition-colors duration-300 p-8 space-y-8 rounded-xl shadow-md border"
@@ -174,19 +174,19 @@ const Page = () => {
 
           {/* Sign Up and Forgot Password */}
           <div className="flex justify-between items-center mt-2">
-            <div className="text-sm">
-              Don{"'"}t have an account?{" "}
-              <Link
-                href="/sign-up"
-                className="text-blue-600 hover:text-blue-800"
-              >
-                Sign Up
-              </Link>
-            </div>
-            <div className="text-red-500 text-sm">
-              <Link href="/forgot-password/email">Forgot Password?</Link>
-            </div>
-          </div>
+  <div className="text-sm">
+    <div>Don{"'"}t have an account?</div>
+    <Link
+      href="/sign-up"
+      className="text-blue-600 hover:text-blue-800"
+    >
+      Sign Up
+    </Link>
+  </div>
+  <div className="text-red-500 text-sm">
+    <Link href="/forgot-password/email">Forgot Password?</Link>
+  </div>
+</div>
 
           {/* Back to Home Button */}
           <div className="text-center mt-4">
