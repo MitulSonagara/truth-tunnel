fc69d98396eeb5989498075a7ff1e2dd7a49343a
Mehul <mehulprajapati2802@gmail.com>
2024-10-31 00:10:15 -0400

    remove unused log

--- src/app/api/keys/route.ts
+++ src/app/api/keys/route.ts
@@ -31,7 +31,6 @@ export async function GET(request: Request) {
         userId: user.id,
       }
     }) as EncryptionKey | null; // Handle possible null
-    console.log("----her 1", keys);
 
     if (!keys) {
       return new Response(JSON.stringify({
